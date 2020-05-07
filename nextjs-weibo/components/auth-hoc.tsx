7330e27886f15439bc4dc370611edf50028c7698
chaos2171053 <chaos2171053@gmail.com>
2020-05-07 19:04:23 +0800

    feat(hoc): auth hoc

--- nextjs-weibo/components/auth-hoc.tsx
+++ nextjs-weibo/components/auth-hoc.tsx
@@ -1,19 +1,45 @@
-import React from 'react'
-import { getValue } from '../utils/localstorage';
+import React, { useEffect } from 'react'
+import { connect } from "react-redux";
+import { UserState, setUserInfo } from '../store/modules/user';
 import { useRouter } from 'next/router'
-function AuthHoc(Compnent) {
-    const router = useRouter()
-    if (!getValue('Token')) {
-        router.push('/sign-in')
-        return;
-    }
-    return (
-        <>
-            <Compnent />
-        </>
-    )
+interface IProps {
+    userInfo: UserState;
+    children?: React.ReactNode;
+    setUserInfo: Function;
+}
+const mapStateToProps = state => ({
+    userInfo: state.user
+});
+const mapDispatchToProps = {
+    setUserInfo
+};
 
+
+function WithAuthHoc(WarpperComponent) {
+
+    function Auth(props: IProps) {
+        const { userInfo, setUserInfo } = props
+        const auths = {
+            userInfo,
+            setUserInfo
+        }
+        const router = useRouter()
+        useEffect(() => {
+            if (!userInfo.token) {
+                router.push('/sign-in')
+            }
+            return () => {
+
+            }
+        }, [userInfo])
+        return (
+            <>
+                <WarpperComponent  {...auths} />
+            </>
+        )
+    }
+
+    return connect(mapStateToProps, mapDispatchToProps)(Auth)
 }
 
-export default AuthHoc
-
+export default WithAuthHoc;
