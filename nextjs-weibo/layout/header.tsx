c24a145ec8c20a4a307e5c4035722f59ad2296a0
chaos2171053 <chaos2171053@gmail.com>
2020-05-19 19:07:46 +0800

    feat:logout

--- nextjs-weibo/layout/header.tsx
+++ nextjs-weibo/layout/header.tsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 import { Navbar, NavDropdown, Nav } from 'react-bootstrap'
-export default function Header(props) {
-  const [selectedNavKey, setSelectedNavKey] = useState('/')
-  const {
-    children
-  } = props;
+import { UserState } from '../store/modules/user';
+import { useRouter } from "next/router";
+interface IProps extends UserState {
+  onLogout: Function
+}
+
+export default function Header(props: IProps) {
+  const { id, name, onLogout } = props
+  const router = useRouter()
+
+  const handleLogout = () => {
+    onLogout()
+    router.replace('/')
+  }
   return (
     <header>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -12,20 +21,29 @@ export default function Header(props) {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
           <Nav>
             <Nav.Link href="/help">Help</Nav.Link>
-            <Nav.Link href="/sign-in">Sign In</Nav.Link>
+            {
+              id ? (
+                <>
+                  <NavDropdown title={name} id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Acount</NavDropdown.Item>
+                    <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item onClick={handleLogout}>Log out</NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                  <>
+                    <Nav.Link href="/sign-in">Sign In</Nav.Link>
+                  </>
+                )
+            }
+
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-    </header>
+    </header >
   );
 }
